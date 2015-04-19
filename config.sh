722c8b70a7f95bfaaeedfb12044f918494ac4796
Simon A. F. Lund <safl@safl.dk>
2015-04-20 01:00:24 +0200

    ...

--- config.sh
+++ config.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 BH_REPOS=~/bohrium
 PT_REPOS=~/perftest
-
-SUITES="perftest_mini_slurm"
--- daily_job.sh
+++ daily_job.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 source config.sh
 
+DATE=`date +%Y_%m_%d`
+SUITES="perftest_mini_slurm"
+
 for SUITE in $SUITES
 do
-    DATE=`date +%Y_%m_%d`
     touch $PT_REPOS/workdir/watch/$SUITE
     echo "${DATE}-01" >> $PT_REPOS/workdir/watch/$SUITE
     echo "${DATE}-02" >> $PT_REPOS/workdir/watch/$SUITE
