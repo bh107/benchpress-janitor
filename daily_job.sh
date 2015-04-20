fc8ff6f265bacd1f124b86b27b3f0c8ba8e383e0
Simon A. F. Lund <safl@safl.dk>
2015-04-20 12:59:05 +0200

    ...

--- daily_job.sh
+++ daily_job.sh
@@ -6,6 +6,7 @@ SUITES="perftest_mini_slurm"
 
 for SUITE in $SUITES
 do
+    echo "Adding ${SUITE} to watch" >> $PT_REPOS/janitor.log
     touch $PT_REPOS/workdir/watch/$SUITE
     echo "${DATE}-01" >> $PT_REPOS/workdir/watch/$SUITE
     echo "${DATE}-02" >> $PT_REPOS/workdir/watch/$SUITE
